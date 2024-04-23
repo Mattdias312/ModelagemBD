64cbefc97a0d2718e0a15120eddcf04956179bf8
Mattdias312 <matheusf.dias12@gmail.com>
2024-04-23 19:56:37 -0300

    locadora

--- Exercício04_locadora.sql
+++ Exercício04_locadora.sql
@@ -21,16 +21,29 @@ on f.COD_FILME=l.COD_FILME group by f.FILME,c.CIDADE having c.CIDADE='Votorantim
 
 --5. Mostre qual filme foi alugado hoje.
 select f.FILME from LOCACOES l inner join FILME f on f.COD_FILME=l.COD_FILME 
-group by l.DATA_RETIRADA,f.FILME having DATA_RETIRADA=GETDATE()
+group by l.DATA_RETIRADA,f.FILME having DATA_RETIRADA=GETDATE();
 
 --6. Mostre quanto à locadora lucrou com as locações do filme “A vida é bela”.
 select SUM(f.VALOR_LOCACAO) as lucro from LOCACOES l inner join FILME f on f.COD_FILME=l.COD_FILME 
-group by f.FILME,f.FILME having DATA_RETIRADA=GETDATE()
+group by f.FILME,f.VALOR_LOCACAO having f.FILME='A vida é bela'
 
 --7. Mostre a quantidade de saídas de filmes para locação agrupando a consulta por diretor.
+select f.DIRETOR,COUNT(l.COD_LOCACAO) as qtde_locacao from CLIENTES c inner join LOCACOES l 
+on c.COD_CLIENTE=l.COD_CLIENTE inner join FILME f
+on f.COD_FILME=l.COD_FILME group by f.DIRETOR;
 
 --8. Mostre todas as categorias dos filmes alugados pela cliente Maria Chiquinha.
+select distinct ca.NOME_CATEGORIA as qtde_locacao from CLIENTES cl inner join LOCACOES l 
+on cl.COD_CLIENTE=l.COD_CLIENTE inner join FILME f
+on f.COD_FILME=l.COD_FILME inner join CATEGORIA ca on ca.COD_CATEGORIA=f.COD_CATEGORIA 
+group by cl.NOME,ca.NOME_CATEGORIA having cl.NOME = 'Maria Chiquinha';
 
 --9. Mostre o nome e o telefone de todos os clientes que já alugaram o filme Constantine.
+select distinct cl.NOME,cl.TELEFONE from CLIENTES cl inner join LOCACOES l 
+on cl.COD_CLIENTE=l.COD_CLIENTE inner join FILME f
+on f.COD_FILME=l.COD_FILME group by cl.NOME,cl.TELEFONE,f.FILME having f.FILME = 'Constantine';
 
 --10. Mostre todas as categorias de filmes que não possuem filmes cadastrado
+select ca.NOME_CATEGORIA from categoria ca where ca.COD_CATEGORIA not in (select ca.COD_CATEGORIA from CATEGORIA ca inner join FILME f
+on ca.COD_CATEGORIA=f.COD_CATEGORIA group by ca.NOME_CATEGORIA,ca.COD_CATEGORIA,f.COD_CATEGORIA
+having ca.COD_CATEGORIA = f.COD_CATEGORIA);
